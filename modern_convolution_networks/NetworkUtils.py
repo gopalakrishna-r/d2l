c32626360108e35dd792d2f0e7a644f65440a84d
Gopalakrishna Ramachandra <3163730+gopalakrishna-r@users.noreply.github.com>
2022-03-03 23:27:42 +0530

    alexnet implementation

--- modern_convolution_networks/NetworkUtils.py
+++ modern_convolution_networks/NetworkUtils.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 from d2l import tensorflow as d2l
-from tensorpack.models import  Conv2D, BatchNorm,MaxPooling, AvgPooling, FullyConnected, Dropout, Flatten
+from tensorpack.models import  Conv2D, BatchNorm,MaxPooling, AvgPooling, FullyConnected,  Flatten, Dropout
 from tensorpack.tfutils.argscope import argscope
 from tensorflow.keras import Input
+from tensorpack.tfutils.common import get_tf_version_tuple
+from tensorpack.tfutils.tower import get_current_tower_context
+from tensorpack.utils.argtools import graph_memoized
+from tensorpack.models.common import layer_register
 
 def alexnet():
     image = Input(shape=(224, 224, 1))
@@ -22,14 +26,13 @@ def alexnet():
         l = Conv2D('conv5', l, filters=256)
         l = MaxPooling('pool1', l)
         l = Flatten('flatten', l)
-        # This is just a script to load model, so we ignore the dropout layer
         l = FullyConnected('fc1', l)
-        l = Dropout('droput1', l, 0.5)
+        l = Dropout('droput1', l , 0.5) # 0.5 by defualt
         l = FullyConnected('fc2', l)
-        l = Dropout('dropout2', l, 0.5)
+        l = Dropout('dropout2', l,0.5)
         
         
-    output_layer =         FullyConnected('FCN3', l , units= 10, activation='softmax')
+    output_layer =         FullyConnected('FCN3', l , units= 10, activation='linear')
         
     return tf.keras.models.Model(image, output_layer)    
         
